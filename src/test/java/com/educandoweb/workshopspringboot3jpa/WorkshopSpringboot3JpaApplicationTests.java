183ad39e03778f654b17e7f0f5bf2b196a358a5c
Jones <jmmarao@gmail.com>
2023-01-29 00:26:17 -0300

    Initialize project

---
+++ src/test/java/com/educandoweb/workshopspringboot3jpa/WorkshopSpringboot3JpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.workshopspringboot3jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringboot3JpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
